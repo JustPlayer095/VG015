3326b1d5e8f5d44a371ab3902be8bac144ac934a
JustPlayer095 <arkady5867@gmail.com>
2026-05-19 13:25:59 +0300

    refactor(osdp): split osdp_context_t into parser and runtime contexts

--- vg015/modules/osdp/core/osdp_parser.h
+++ vg015/modules/osdp/core/osdp_parser.h
@@ -4,9 +4,9 @@
 #include <stdint.h>
 #include "../state/osdp_context.h"
 
-typedef void (*osdp_parser_frame_cb_t)(osdp_context_t *ctx, const uint8_t *frame, uint16_t frame_len);
+typedef void (*osdp_parser_frame_cb_t)(osdp_parser_ctx_t *ctx, const uint8_t *frame, uint16_t frame_len);
 
-void osdp_parser_reset(osdp_context_t *ctx);
-void osdp_parser_on_byte(osdp_context_t *ctx, uint8_t byte, osdp_parser_frame_cb_t cb);
+void osdp_parser_reset(osdp_parser_ctx_t *ctx);
+void osdp_parser_on_byte(osdp_parser_ctx_t *ctx, uint8_t byte, osdp_parser_frame_cb_t cb);
 
 #endif
